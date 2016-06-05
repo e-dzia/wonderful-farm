22a26be7c7bd14fddc343b86ca5be519b9f9a670
e-dzia <226000@student.pwr.edu.pl>
2016-06-05 19:38:53 +0200

    h

--- main.cpp
+++ main.cpp
@@ -6,25 +6,6 @@
 #include "Herd.h"
 #include "action.h"
 
-void randomIncidents(Herd&, Herd&, Herd&, Herd&, Herd&, Herd&, Herd&);
-void clear();
-void pause();
-int start();
-void menu(double&, Herd&, Herd&, Herd&, Herd&, Herd&, Herd&, Herd&, int&, int&, int&, int&, int&, int&, int&);
-void period(double&, int, Herd&, Herd&, Herd&, Herd&, Herd&, Herd&, Herd&, int&, int&, int&, int&, int&, int&, int&);
-void summary(double&, Herd&, Herd&, Herd&, Herd&, Herd&, Herd&, Herd&);
-void bankruptcy(double&, Herd&, Herd&, Herd&, Herd&, Herd&, Herd&, Herd&);
-int isCorrect(int);
-
-/*
-- uzytkownik wybiera liczbe lat symulacji
-- menu do podejmowania decyzji w sprawie farmy (co chce kupic/sprzedac)
-- petla do liczba_lat, w kazdym obrocie powiekszaja sie stada, mozna sprzedawac zwierzeta/produkty, kupować zwierzeta. Przy kazdym obrocie musi
-wyswietlac sie info: ile jakich zwierzat i jakiej plci ma uzytkownik, ile roslin, ile ma hajsu i czy ktos atakowal jego zwierzeta albo ukradł :p
-- w każdym obrocie mogą zdarzyć się zdarzenia losowe - drapieżnik/złodziej -> usuwa losowe osobniki i wyswietla info, że zabrał kilka osobnikow
-- zwierzeta przechowywane sa w vectorze (lepiej niz w kolejce, bo mozna usuwac srodkowe elementy tez)
-*/
-
 using namespace std; 
 
 int main(int argc, char** argv){
